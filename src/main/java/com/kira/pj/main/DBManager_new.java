9fabb3afdc484747792598fa09fe6d7daf0dc79f
DESKTOP-K3VU0LA\soldesk <kshe1242@naver.com>
2026-03-31 17:17:18 +0900

    gradle set / db set

---
+++ src/main/java/com/kira/pj/main/DBManager_new.java
@@ -0,0 +1,39 @@
+package com.kira.pj.main;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DBManager_new {
+    private static BasicDataSource dataSource;
+    static {
+        dataSource = new BasicDataSource();
+        dataSource.setUrl("jdbc:oracle:thin:@10.1.82.127:1521:XE");
+        dataSource.setUsername("c##kira");
+        dataSource.setPassword("kira1004");
+        dataSource.setMinIdle(10);        // 최소 유휴 커넥션
+        dataSource.setMaxIdle(20);        // 최대 유휴 커넥션
+        dataSource.setMaxOpenPreparedStatements(100); // 풀에서 열린 최대 준비된 sql문 개수
+
+    }
+    public static Connection connect() throws SQLException {
+        return dataSource.getConnection();
+    }
+    public static void close(Connection con, PreparedStatement pstmt, ResultSet rs) {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+            pstmt.close();
+            if (con != null) {
+                con.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
